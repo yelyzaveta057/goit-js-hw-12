312ebf5f72d63461cf4aa90996cb26a1866846a2
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-04-15 23:53:08 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,9 +6,18 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loaderMore = document.querySelector('.loader-load');
+const btnLoad = document.querySelector('.btn-load');
+
+loader.style.display = 'none';
+btnLoad.style.display = 'none';
+loaderMore.style.display = 'none';
+
+let currentQuery = '';
+let totalHits = 0;
+let imagesLoaded = 0;
 
 // лоадер спочатку прихований
-loader.style.display = 'none';
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   const query = input.value.trim();
@@ -17,10 +26,13 @@ form.addEventListener('submit', function (event) {
     return;
   }
 
-  gallery.innerHTML = ''; // очищаємо галерею перед новим пошуком
-  loader.style.display = 'block'; // показуємо лоадер
+  resetPage();
+  gallery.innerHTML = '';
+  btnLoad.style.display = 'none';
+  loader.style.display = 'block';
+  currentQuery = query;
 
-  searchImages(query)
+  searchImages(currentQuery)
     .then(images => {
       loader.style.display = 'none'; // ховаємо лоадер після отримання результатів
       if (images.length === 0) {
@@ -29,12 +41,64 @@ form.addEventListener('submit', function (event) {
         );
         return;
       }
+
+      totalHits = hits;
+      imagesLoaded = images.length;
+
       updateGallery(images);
+
+      if (imagesLoaded < totalHits) {
+        btnLoad.style.display = 'block';
+      } else {
+        btnLoad.style.display = 'none'; // Якщо тільки 1 фото, ховаємо кнопку
+        showNoResultsMessage(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
     })
     .catch(error => {
       loader.style.display = 'none';
       showNoResultsMessage('Error fetching images. Please try again!');
       console.error('Помилка сервера:', error.message);
     });
+
   form.reset();
+});
+
+btnLoad.addEventListener('click', async () => {
+  if (currentQuery === '') return;
+
+  loaderMore.style.display = 'block';
+  btnLoad.style.display = 'none';
+
+  await searchImages(currentQuery)
+    .then(({ images }) => {
+      loaderMore.style.display = 'none';
+
+      if (images.length === 0) {
+        showNoResultsMessage(
+          "We're sorry, but you've reached the end of search results."
+        );
+        return;
+      }
+
+      updateGallery(images);
+      smoothScroll();
+
+      imagesLoaded += images.length;
+
+      if (imagesLoaded >= totalHits) {
+        btnLoad.style.display = 'none';
+        showNoResultsMessage(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        btnLoad.style.display = 'block';
+      }
+    })
+    .catch(error => {
+      loaderMore.style.display = 'none';
+      showNoResultsMessage('Error loading more images.');
+      console.error('Помилка сервера:', error.message);
+    });
 });
